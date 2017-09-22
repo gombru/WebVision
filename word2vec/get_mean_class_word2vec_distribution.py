7cb840b894395f7d423eed12819961dd336e6873
raulgomez <raulgombru@gmail.com>
2017-09-22 14:32:00 +0200

    resnet

--- word2vec/get_mean_class_word2vec_distribution.py
+++ word2vec/get_mean_class_word2vec_distribution.py
@@ -98,6 +98,9 @@ def infer_word2vec(d):
         if np.isnan(embedding).any():
             print "Wrong embedding"
             return
+        if sum(embedgin) < 0.00001:
+            print "0 embedding"
+            return
 
         embedding = embedding / sum(embedding)
 
